b8ec0515ae901800d03c9aa10513e85b50c6c60c
Tom Jahncke <Tom.Jahncke@gfs.com>
2018-05-08 18:10:41 -0400

    Created TJBot

--- battle-stage.js
+++ battle-stage.js
@@ -5,11 +5,12 @@ import AdaptiveDefender from './example-bots/adaptive-defender.js';
 import ZergRusher from './example-bots/zerg-rush.js';
 import HardHittingPacifist from './example-bots/hard-hitting-pacifist.js';
 import Randomonium from './example-bots/random-choices.js';
+import TJBot from './bots/tawj.js';
 
 let match = new Match({
   game: new GameOfDrones({ rounds: 1000 }),
-  player1: Randomonium(),
-  player2: ZergRusher()
+  player1: AdaptiveDefender(),
+  player2: TJBot()
 });
 
 console.log(JSON.stringify(match.play(), null, 2));
